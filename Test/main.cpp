d1b537257432518d627693e7a22f9771dc4313fa
Konstantinos <58148103+konmaz@users.noreply.github.com>
2020-04-06 11:01:13 +0300

    Add text file

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
 using namespace std;
+void remove_gar(string &s)
+{
+    int x;
+}
+
+
 int main()
 {
-    ifstream file("inputfile.txt");
+    vector<string> ArrayWithWords;
+    size_t d = 0;
+
+
+    ifstream file("input_file.txt");
     string linestr;
     while (getline(file, linestr))
     {
-        cout<<linestr<<endl;
+        //remove_gar(linestr);
+        ArrayWithWords.push_back(linestr);
+        d++;
+        //cout<<linestr<<endl;
     }
+
+
+    for (int i = 0; i < ArrayWithWords.size(); i++)
+    {
+        cout<<ArrayWithWords[i]<<"                                      With strlen =  "<<ArrayWithWords[i].size()<<endl;
+        d+=ArrayWithWords[i].size();
+
+    }
+    cout<<"\n\n\n-------------------------------"<<endl;
+    cout<<"The number of lines is "<<ArrayWithWords.size()<<endl;
+    cout<<"Total number of characters = "<<d<<endl;
 }
